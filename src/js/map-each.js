730eebbca045280f4e448b09a488ec87d9043b45
singcl <24661881@qq.com>
2018-02-05 11:57:59 +0800

    map-each.js测试用例待添加！⁉

--- src/js/map-each.js
+++ src/js/map-each.js
@@ -1,15 +1,15 @@
 // Array原型上map和forEach方法基本原理
 /*eslint-disable no-unused-vars*/
-function each (arr, callback) {
-  for (var i = 0; i < arr.length; i++) {
-    callback && callback(arr[i], i, arr)
-  }
+function each(arr, callback) {
+    for (var i = 0; i < arr.length; i++) {
+        callback && callback(arr[i], i, arr);
+    }
 }
 
-function map (arr, callback) {
-  let list = []
-  for (var i = 0; i < arr.length; i++) {
-    list.push(callback && callback(arr[i], i, arr))
-  }
-  return list
+function map(arr, callback) {
+    let list = [];
+    for (var i = 0; i < arr.length; i++) {
+        list.push(callback && callback(arr[i], i, arr));
+    }
+    return list;
 }
