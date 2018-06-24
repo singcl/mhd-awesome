9d5da7f304969b1f05856a7609a103d517b38e81
singcl <24661881@qq.com>
2018-06-24 18:00:28 +0800

    :art: 异步迭代使用环境说明

--- src/md/async_generator.js
+++ src/md/async_generator.js
@@ -1,4 +1,6 @@
-var fs = require('fs');
+var fs = require("fs");
+// 运行环境: Nodejs 10.x
+// for-await-of 必须存在于异步函数中,在异步函数外使用会报错
 
 ///////////////////// 传统的流的读取方法/////////////////////////////////
 /* const stream = fs.createReadStream(filepath, {
@@ -16,8 +18,8 @@ stream.on('end', () => {
 // 创建一个可读流
 // Node 10.x ExperimentalWarning: Readable[Symbol.asyncIterator]
 // @see https://zaiste.net/nodejs_10_asynchronous_iteration_async_generators/
-const stream = fs.createReadStream('./beego_hello.go', {
-    encoding: 'utf8', // null -> buffers, 'utf8' -> strings with that encoding
+const stream = fs.createReadStream("./beego_hello.go", {
+    encoding: "utf8", // null -> buffers, 'utf8' -> strings with that encoding
     highWaterMark: 1024 // maximum size of each chunk (buffer or string)
 });
 
@@ -26,7 +28,7 @@ const stream = fs.createReadStream('./beego_hello.go', {
     for await (const chunk of stream) {
         console.log(`Read: ${chunk}`);
     }
-    console.log('EOF');
+    console.log("EOF");
 })();
 
 // 异步生成器函数 返回一个异步生成器 asyncIterator
@@ -39,18 +41,18 @@ async function* fromLinesToNumberedLines(lines) {
 }
 
 // 1. 手动调用异步生成器对象的next方法，返回Promise<IterationResult>
-fromLinesToNumberedLines(fs.createReadStream('./fibonacci.js'))
+fromLinesToNumberedLines(fs.createReadStream("./fibonacci.js"))
     .next()
-    .then((r) => {
+    .then(r => {
         console.log(r);
     });
 
 // 2. fow await of 自动迭代 异步可迭代对象
 (async function() {
     for await (let v of fromLinesToNumberedLines(
-        fs.createReadStream('./app.js')
+        fs.createReadStream("./app.js")
     )) {
-        console.log('自动迭代：', v);
+        console.log("自动迭代：", v);
     }
 })();
 
@@ -69,20 +71,20 @@ async function* genAnswers() {
 // 定义一个递归遍历生成器对象的函数
 // 貌似是一个尾递归
 function forEach(asyncIterator, callback) {
-    return asyncIterator.next().then((r) => {
+    return asyncIterator.next().then(r => {
         if (!r.done) {
             callback(r);
             return forEach(asyncIterator, callback);
         }
-        return 'Hello, Singcl';
+        return "Hello, Singcl";
     });
 }
 
 var output = 0;
 forEach(genAnswers(), function(v) {
     output += v.value;
-}).then((v) => {
-    console.log(v, ',The result output is ', output);
+}).then(v => {
+    console.log(v, ",The result output is ", output);
 });
 
 // 示例2：@link: https://segmentfault.com/a/1190000013387616
