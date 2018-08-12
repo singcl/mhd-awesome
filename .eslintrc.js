92e3a8a1a2e19131948f9fc67069df9ba0dc5a41
singcl <24661881@qq.com>
2018-08-12 12:38:36 +0800

    :lint: 增加semi规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-  "extends": "eslint:recommended",
-  "plugins": [],
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
-  },
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module"
-  },
-  "globals": { "ActiveXObject": true },
-  "rules": {
-  	"no-console": 0
-  }
+    extends: "eslint:recommended",
+    plugins: [],
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module"
+    },
+    globals: { ActiveXObject: true },
+    rules: {
+        "no-console": 0,
+        semi: ["warn", "never"]
+    }
 }
